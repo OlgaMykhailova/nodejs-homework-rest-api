cb4c5c24753206a1b843b80e0effcbaddd58d07c
OlgaMykhailova <rosebud_z@yahoo.com>
2023-01-11 15:22:42 +0100

    added login+token

---
+++ controllers/users/login.js
@@ -0,0 +1,36 @@
+const createError = require("http-errors");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const { User } = require("../../models");
+
+const { SECRET_KEY } = process.env;
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+    
+  if (!user || !bcrypt.compareSync(password, user.password)) {
+    throw createError(401, "message: Email or password is wrong");
+  }
+
+  const payload = {
+    id: user._id,
+  };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
+
+  res.json({
+    status: "200 OK",
+    code: 200,
+    data: {
+      token,
+      user: {
+        email: user.email,
+        subscription: user.subscription,
+      },
+    },
+  });
+};
+
+module.exports = login;
--- controllers/users/signup.js
+++ controllers/users/signup.js
@@ -5,22 +5,27 @@ const { User } = require("../../models");
 
 const signup = async (req, res) => {
   const { email, password, subscription } = req.body;
-  console.log(User)
+
   const user = await User.findOne({ email });
+
   if (user) {
     throw createError(409, "message: Email in use");
   }
 
   const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 
-  const result = await User.create({ email, password: hashPassword, subscription });
+  const result = await User.create({
+    email,
+    password: hashPassword,
+    subscription,
+  });
   res.status(201).json({
     status: "201 Created",
     code: 201,
     data: {
       user: {
         email: result.email,
-        subscription: result.subscription
+        subscription: result.subscription,
       },
     },
   });
